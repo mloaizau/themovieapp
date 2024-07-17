f9cc3c59e49d9498fc70a2a60dd06db1c645ff9d
mloaizau <mixi7395@gmail.com>
2024-07-17 14:29:22 -0400

    install navigations

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Button, PaperProvider } from 'react-native-paper';
 import { SafeAreaView, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 function App(): React.JSX.Element {
 
   return (
     <PaperProvider>
-      <SafeAreaView>
-        <Button style={{width: "80%", marginHorizontal:40}} icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-    Press me
-  </Button>
-      </SafeAreaView>
+      <NavigationContainer>
+        <SafeAreaView>
+          <Button style={{ width: "80%", marginHorizontal: 40 }} icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+            Press me
+          </Button>
+        </SafeAreaView>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
